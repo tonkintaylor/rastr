f9a5bfcf6522f6de5426b996f1e5290fbc94dedd
Nathan McDougall <nmcdougall@tonkintaylor.co.nz>
2025-08-19 18:01:55 +1200

    Make `crs` kwarg-only in `read_raster_inmem`

--- src/rastr/io.py
+++ src/rastr/io.py
@@ -15,7 +15,9 @@ if TYPE_CHECKING:
     from numpy.typing import NDArray
 
 
-def read_raster_inmem(raster_path: Path | str, crs: CRS | None = None) -> RasterModel:
+def read_raster_inmem(
+    raster_path: Path | str, *, crs: CRS | None = None
+) -> RasterModel:
     """Read raster data from a file and return an in-memory Raster object."""
     with rasterio.open(raster_path, mode="r") as dst:
         # Read the entire array

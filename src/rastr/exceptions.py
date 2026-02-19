36daae0536eae2f5c32ac4de5b77fe73943d50ec
Ben Karl <benjames.karl@gmail.com>
2026-02-20 10:05:42 +1300

    introduce base exception class RastrError for improved exception hierarchy

--- src/rastr/exceptions.py
+++ src/rastr/exceptions.py
@@ -1,5 +1,9 @@
 """Custom exceptions for the rastr package."""
 
 
-class NonSquareCellsError(ValueError):
+class RastrError(Exception):
+    """Base exception for the rastr package."""
+
+
+class NonSquareCellsError(ValueError, RastrError):
     """Raised when square cells are required but the raster has non-square cells."""
